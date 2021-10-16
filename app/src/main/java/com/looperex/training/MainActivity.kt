7e296fa8784c66f73a51b2200640f5b1426b0d02
venkatachalam <venkatachalam@bitcot.com>
2021-10-16 11:59:43 +0530

    List View OnClick Listener Implemented

--- app/src/main/java/com/looperex/training/MainActivity.kt
+++ app/src/main/java/com/looperex/training/MainActivity.kt
@@ -3,22 +3,12 @@ package com.looperex.training
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity: AppCompatActivity()  {
 
-    /*array
-    *
-    * Apple
-    * Banana
-    * Cherry
-    * Orange
-    * mango
-    * watermelon
-    * kiwi
-    *
-    * */
-
+    //Fruits List - Array List variable
     var fruits: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +19,7 @@ class MainActivity: AppCompatActivity()  {
 
     private fun initViews() {
 
+        //Adding data into ListView
         fruits.add("Apple")
         fruits.add("Banana")
         fruits.add("Cherry")
@@ -48,7 +39,16 @@ class MainActivity: AppCompatActivity()  {
         fruits.add("Kiwi")
         fruits.add("Mango")
 
-        myListView.adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,fruits)
+        //Creating an Array Adapter object and storing in the variable named adapterObj
+        var adapterObj = ArrayAdapter(this,R.layout.mylistitem,fruits)
+
+        //Setting an Array Adapter for ListView named myListView
+        myListView.adapter = adapterObj
+
+        //List view Item Click Listener and showing Toast
+        myListView.setOnItemClickListener { parent, view, position, id ->
+            Toast.makeText(this, fruits.get(position), Toast.LENGTH_LONG).show()
+        }
 
 
     }
